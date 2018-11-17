6a68deced08760b1344d9f63e641c26c2f5a9c05
fathy <fathy@DESKTOP-KL6HPHJ>
2018-11-17 17:05:28 +0200

    add jpa annotation for transactional process for save operation

--- OnlineBanking/src/main/java/com/Bank/demo/dao/AccountDao.java
+++ OnlineBanking/src/main/java/com/Bank/demo/dao/AccountDao.java
@@ -1,11 +1,14 @@
 package com.Bank.demo.dao;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.Bank.demo.model.Account;
 
 @Repository
+@Transactional
 public interface AccountDao extends CrudRepository<Account, Long> {
 
 	public Account findAccountByBalance(double balance);
